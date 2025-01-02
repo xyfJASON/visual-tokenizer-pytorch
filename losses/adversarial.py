91c4b062182e579f53797f09c537b4255ec98e57
xyfJASON <xyfjason@163.com>
2025-01-02 13:49:55 +0800

    Add titok (wip)

--- losses/adversarial.py
+++ losses/adversarial.py
@@ -16,12 +16,30 @@ class AdversarialLoss(nn.Module):
         Objective of the generator: min -E[D(G(z))]
 
     """
-    def __init__(self, discriminator: nn.Module, loss_type: str):
+    def __init__(
+            self,
+            discriminator: nn.Module,
+            loss_type: str,
+            coef_lecam_reg: float = 0.0,
+            lecam_reg_ema_decay: float = 0.999,
+    ):
         super().__init__()
         assert loss_type in ['ns', 'hinge']
 
         self.discriminator = discriminator
         self.loss_type = loss_type
+        self.coef_lecam_reg = coef_lecam_reg
+        if self.coef_lecam_reg > 0.0:
+            self.lecam_reg_ema_decay = lecam_reg_ema_decay
+            self.register_buffer('ema_real_logits_mean', torch.zeros(1))
+            self.register_buffer('ema_fake_logits_mean', torch.zeros(1))
+
+    def lecam_reg(self, real_logits_mean: Tensor, fake_logits_mean: Tensor):
+        lecam_loss = (torch.mean(torch.pow(F.relu(real_logits_mean - self.ema_fake_logits_mean), 2)) +
+                      torch.mean(torch.pow(F.relu(self.ema_real_logits_mean - fake_logits_mean), 2)))
+        self.ema_real_logits_mean = self.ema_real_logits_mean * self.lecam_reg_ema_decay + real_logits_mean.detach() * (1 - self.lecam_reg_ema_decay)  # noqa
+        self.ema_fake_logits_mean = self.ema_fake_logits_mean * self.lecam_reg_ema_decay + fake_logits_mean.detach() * (1 - self.lecam_reg_ema_decay)  # noqa
+        return lecam_loss
 
     def forward_G(self, fake_data: Tensor, *args, **kwargs):
         fake_logits = self.discriminator(fake_data, *args, **kwargs)
@@ -45,6 +63,8 @@ class AdversarialLoss(nn.Module):
         else:
             raise ValueError(f'Unknown loss type: {self.loss_type}')
 
+        if self.coef_lecam_reg > 0.0:
+            loss = loss + self.coef_lecam_reg * self.lecam_reg(real_logits.mean(), fake_logits.mean())
         return loss
 
     def forward(self, mode: str, fake_data: Tensor, real_data: Tensor = None, *args, **kwargs):
